20b3c2b4b39e846b7934d110ced441dea3785477
yusuf <yoesuv@hotmail.co.id>
2016-10-28 10:01:42 +0700

    Add index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Index</title>
+    </head>
+    <body>
+        <h1>Learning Git</h1>
+        <h2>Hello World</h2>
+    </body>
+</html>
